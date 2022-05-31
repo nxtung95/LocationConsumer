12073c0090aaf6de9cbaac2bed6a869aef20b720
tungnx <tungnx@vnpay.vn>
2022-05-31 17:36:04 +0700

    up

--- src/java/consumer/LocationConsumer.java
+++ src/java/consumer/LocationConsumer.java
@@ -1,19 +1,16 @@
 package consumer;
 
-import jakarta.annotation.Resource;
 import jakarta.ejb.ActivationConfigProperty;
 import jakarta.ejb.MessageDriven;
-import jakarta.ejb.MessageDrivenContext;
-import jakarta.jms.JMSException;
 import jakarta.jms.Message;
 import jakarta.jms.MessageListener;
 import jakarta.jms.TextMessage;
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @MessageDriven(activationConfig =
@@ -30,57 +27,48 @@ import java.util.logging.Logger;
       propertyValue = "jakarta.jms.Queue")
 })
 public class LocationConsumer implements MessageListener {
-   // field obtained via dependency injection (not used here)
-   @Resource
-   private MessageDrivenContext mdc;
    private Logger logger;
-   Socket socket = null;
-   
 
    public LocationConsumer() {
       logger = Logger.getLogger(getClass().getName());
-      init();
-   }
-   
-   public void init() {
-       try {
-            ServerSocket serverSocket = new ServerSocket(6543);
-            logger.info("MessageSendingServer start, waiting for all client open connection...");
-            socket = serverSocket.accept();
-            String clientIp = socket.getLocalAddress().getHostAddress();
-            int clientPort = socket.getPort();
-            logger.info("Server accept, client_ip: " + clientIp + ", port: " + clientPort);  
-       } catch (IOException e) {
-           
-       }
    }
 
    @Override
    public void onMessage(Message message){
-        BufferedReader bufferReader = null;
+       System.out.println("Consume message: " + message);
         BufferedWriter bufferWriter = null;
         try {
-            if (message instanceof TextMessage) {
-               String mess = ((TextMessage) message).getText();
-               logger.info("Consumer received text message: " + mess);
+            if ((message instanceof TextMessage)) {
+                try {
+                    ServerSocket serverSocket = new ServerSocket(6543);
+                    Socket socket = serverSocket.accept();
+                    String clientIp = socket.getLocalAddress().getHostAddress();
+                    int clientPort = socket.getPort();
+                    logger.info("Server accept, client_ip: " + clientIp + ", port: " + clientPort); 
+                     
+                    TextMessage textMessage = (TextMessage) message;
+                    logger.info("Consumer received text message: " + textMessage.getText());
 
-               bufferWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-               logger.info("Send all clients, message: " + mess);
+                    bufferWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                    logger.info("Send all clients, message: " + textMessage.getText());
 
-               bufferWriter.write(mess);
-               bufferWriter.newLine();
-               bufferWriter.flush();
+                    bufferWriter.write(textMessage.getText());
+                    bufferWriter.newLine();
+                    bufferWriter.flush();
+                     
+                } catch (IOException e) {
+                    logger.warning("IOException: " + e);
+                }
             }
-        } catch (JMSException e) {
-           logger.warning("Exception with incoming message: " + e);
         } catch (Exception e) {
-            logger.warning("Exception " + e);
+           logger.warning("Exception with incoming message: " + e);
         } finally {
-            try {
-                bufferReader.close();
-                bufferWriter.close();
-            } catch (IOException ex) {
-
+            if (bufferWriter != null) {
+                try {
+                    bufferWriter.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(LocationConsumer.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
         }
    }
